c52c5eb529eff0f234a1a3cb1556acc9ab68dbf0
matthieuczeb-lab <matthieu.czeb@gmail.com>
2026-02-13 20:05:23 +0100

    Premier commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Bot connecté en tant que {client.user}")
+
+client.run(TOKEN)
